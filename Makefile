8b135a66294d19e96ec4b84c0c8c91d87ff74bd3
bielski <bielski@fastmail.net>
2015-11-21 10:40:03 +0100

    one program for testing parts of qemu code

--- Makefile
+++ Makefile
@@ -3,7 +3,10 @@ INC=$(shell pkg-config --cflags glib-2.0)
 LIBS=$(shell pkg-config --libs glib-2.0)
 
 %: %.c
-	${CC} -std=c99 -o $@ $< -lncurses
+	${CC} -g -std=c99 -o $@ $< -lncurses
+
+qemu_test: qemu_test.c
+	${CC} ${INC} -g -std=c99 -o $@ $< ${LIBS}
 
 calc_parser: calc_parser.c
 	${CC} ${INC} -g -std=c99 -o $@ $< ${LIBS}
@@ -18,6 +21,5 @@ threadCleanup: threadCleanupHandler.cpp
 	g++ -Wall -o $@ $^ -pthread
 
 clean:
-	@-rm -rf flexThreads
-	@-rm -rf threadCleanup
+	@-rm -rf flexThreads calc_parser threadCleanup
 	@-rm -rf hello_ncurses macros calc_parser
---
+++ qemu_test.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <glib.h>
+#include <string.h>
+
+/* testing elements of QEMU code */
+typedef struct InetSocketAddress
+{
+    char *host;
+    char *port;
+    bool has_to;
+    uint16_t to;
+    bool has_ipv4;
+    bool ipv4;
+    bool has_ipv6;
+    bool ipv6;
+} InetSocketAddress;
+
+InetSocketAddress *inet_parse(const char *str)
+{
+    InetSocketAddress *addr;
+    const char *optstr, *h;
+    char host[64];
+    char port[33];
+    int to;
+    int pos;
+
+    addr = g_new0(InetSocketAddress, 1);
+
+    /* parse address */
+    if (str[0] == ':') {
+        /* no host given */
+        host[0] = '\0';
+        if (1 != sscanf(str, ":%32[^,]%n", port, &pos)) {
+            fprintf(stderr, "error parsing port in address '%s'", str);
+            goto fail;
+        }
+    } else if (str[0] == '[') {
+        /* IPv6 addr */
+        if (2 != sscanf(str, "[%64[^]]]:%32[^,]%n", host, port, &pos)) {
+            fprintf(stderr, "error parsing IPv6 address '%s'", str);
+            goto fail;
+        }
+        addr->ipv6 = addr->has_ipv6 = true;
+    } else {
+        /* hostname or IPv4 addr */
+        if (2 != sscanf(str, "%64[^:]:%32[^,]%n", host, port, &pos)) {
+            fprintf(stderr, "error parsing address '%s'", str);
+            goto fail;
+        }
+        if (host[strspn(host, "0123456789.")] == '\0') {
+            addr->ipv4 = addr->has_ipv4 = true;
+        }
+    }
+
+    addr->host = g_strdup(host);
+    addr->port = g_strdup(port);
+
+    /* parse options */
+    optstr = str + pos;
+    h = strstr(optstr, ",to=");
+    if (h) {
+        h += 4;
+        if (sscanf(h, "%d%n", &to, &pos) != 1 ||
+            (h[pos] != '\0' && h[pos] != ',')) {
+            fprintf(stderr, "error parsing to= argument");
+            goto fail;
+        }
+        addr->has_to = true;
+        addr->to = to;
+    }
+    if (strstr(optstr, ",ipv4")) {
+        addr->ipv4 = addr->has_ipv4 = true;
+    }
+    if (strstr(optstr, ",ipv6")) {
+        addr->ipv6 = addr->has_ipv6 = true;
+    }
+    return addr;
+
+fail:
+    return NULL;
+}
+
+int main(int argc, char** argv)
+{
+    InetSocketAddress *isa = NULL;
+    isa = inet_parse("127.0.0.1:4545");
+    return 0;
+}
