c180f9071e975b65824c60aeed0dfa0578b30284
Maciej Bielski <bielski.maciek@gmail.com>
2020-04-16 19:02:25 +0200

    moved hello_pro out

--- hello_pro/include/common.hh
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-namespace enum_class { int main(); }
-
-namespace virt_funs { int main(); }
--- hello_pro/src/linkage_test.cc
+++
@@ -1,5 +0,0 @@
-/* For test only, it should be in a namespace anyway */
-#include <string>
-
-// `extern` mandatory for the variable to be visible outside
-extern const std::string global_label = "External variable";
